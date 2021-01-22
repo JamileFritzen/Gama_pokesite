938c6c6dc5f6dfeaa4ffa91b99cd4355d1f498ef
JamileFritzen <75860477+JamileFritzen@users.noreply.github.com>
2021-01-21 22:45:03 -0300

    Update PokemonCard.js

--- PokemonCard.js
+++ PokemonCard.js
@@ -9,6 +9,22 @@ export default class PokemonCard extends Component {
 
     // ta aparecendo so uma parte dos pokemon, ainda nao sei pq
 
+    componentDidMount(){
+        const {name, url}=this.props;
+        const prePokemonIndex = url.split('/')[url.split('/').length-2];
+        //const pokemonIndex=`00${prePokemonIndex}`
+        if (prePokemonIndex<10){
+            var pokemonIndex=`00${prePokemonIndex}`;} else if (prePokemonIndex<100){
+             var pokemonIndex=`0${prePokemonIndex}`;} else {var pokemonIndex=prePokemonIndex}
+        const imageUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${pokemonIndex}.png`
+        
+        this.setState({
+            name,
+            imageUrl,
+            pokemonIndex
+        });
+    }
+
     render(){
         const name = this.props.name;
         const url = this.props.url;
@@ -17,7 +33,9 @@ export default class PokemonCard extends Component {
             <div className="col-md-3 col-sm-6 mb-5"> 
                 <div className="card">
                     <div className="cardHeader"></div>
-                    <h1>{name}</h1>
+                    <img src={this.state.imageUrl}/>
+                    <h2>{name}</h2>
+                    <h5>{this.state.pokemonIndex}</h5>
                 </div>
             </div>
         )
